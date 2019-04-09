910fece0cb5552ce5f4ac0a48506b43e02bdd82e
naveenamadepally13 <47007814+naveenamadepally13@users.noreply.github.com>
2019-04-08 23:19:20 -0500

    Added data storage in cookies and data retrieval from cookies

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
-const app = express();
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
+const app = express();
+app.use(cookieParser());
+// support parsing of application/json type post data
+app.use(bodyParser.json());
+
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
 const server = app.listen(7000, () => {
     console.log(`Express running → PORT ${server.address().port}`);
 });
@@ -10,4 +18,18 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.render('index');
+});
+
+app.post('/home', (req, res, next) => {
+  let userData = {
+        email : req.body.email,
+        password : req.body.password
+    };
+    res.cookie(req.body.email,userData);
+    res.render('home');
+});
+//Iterate users data from cookie
+app.get('/getuser', (req, res)=> {
+//shows all the cookies
+    res.render('home', {user: JSON.stringify(req.cookies)})
 });
